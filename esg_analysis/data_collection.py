058d9172fad1c63bed7a5cb610f23927e3bbde6b
emrecncelik <emrecan.celik@etiya.com>
2022-04-10 02:25:19 +0300

    changed everything idk the specifics anymore

--- esg_analysis/data_collection.py
+++ esg_analysis/data_collection.py
@@ -30,30 +30,56 @@ class ESGNews(GNews):
         self,
         keys: list[str] = [],
         keys_exact_match: list[str] = [],
-        return_df: bool = True,
     ):
         if not keys and not keys_exact_match:
             raise ValueError("One of keys or keys_exact_match is required")
 
         logging.info("Getting metadata for articles.")
         all_keys = []
-        keys_exact_match = ['"' + key + '"' for key in keys_exact_match]
         all_keys.extend(keys)
-        all_keys.extend(keys_exact_match)
-
-        search_term = "%20".join(all_keys)
+        search_term = "%20".join(keys)
         url = BASE_URL + "/search?q={}".format(search_term) + self._ceid()
+        logging.info(f"Search URL: {url}")
 
-        news = self._get_news(url)
+        news = pd.DataFrame(self._get_news(url))
+        news = self._get_news_article_texts(news, keys_exact_match)
+        return pd.DataFrame(news)
 
-        if return_df:
-            return pd.DataFrame(news)
-        else:
-            return news
-
-    def get_news_article_texts(self, urls: list[str]):
+    def _get_news_article_texts(
+        self, news: pd.DataFrame, keys_exact_match: list[str] = []
+    ):
         logger.info("Getting articles from URLS.")
-        articles = [self.get_full_article(url) for url in tqdm(urls)]
-        logger.info("Extracting texts from articles.")
-        texts = [article.text for article in tqdm(articles)]
-        return texts
+        texts = []
+
+        for url in tqdm(news["url"]):
+            try:
+                texts.append(self.get_full_article(url).text)
+            except Exception as err:
+                logger.error(err.args[0])
+                texts.append(pd.NA)
+
+        news["text"] = texts
+        news = news.dropna()
+        valid_rows = []
+        for _, row in news.iterrows():
+            is_valid = all(
+                [
+                    True if key in row["text"].lower() else False
+                    for key in keys_exact_match
+                ]
+            )
+            if is_valid:
+                valid_rows.append(row)
+
+        filtered_news = pd.DataFrame(valid_rows)
+        filtered_news.columns = news.columns
+        return filtered_news
+
+
+if __name__ == "__main__":
+
+    google_news = ESGNews(language="en", country="US", period=None, max_results=30)
+
+    news = google_news.get_news_multi_keywords(
+        keys_exact_match=["tesla"],
+    )
